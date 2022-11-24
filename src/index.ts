a13f0c75595174e9ed1916b9c08aa31f9ccb940d
Simsek Mert <simsek97@gmail.com>
2022-11-24 14:35:02 -0500

    Update imports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import * as appflow from './api/appflow';
-import * as cloudformation from './api/cloudformation';
-import * as cloudwatch from './api/cloudwatch';
-import * as iam from './api/iam';
-import * as s3 from './api/s3';
-import * as secretsmanager from './api/secretsmanager';
-import * as sfn from './api/sfn';
-import * as sns from './api/sns';
+import * as appflow from 'api/appflow';
+import * as cloudformation from 'api/cloudformation';
+import * as cloudwatch from 'api/cloudwatch';
+import * as iam from 'api/iam';
+import * as s3 from 'api/s3';
+import * as secretsmanager from 'api/secretsmanager';
+import * as sfn from 'api/sfn';
+import * as sns from 'api/sns';
 
 export { appflow, cloudformation, cloudwatch, iam, s3, secretsmanager, sfn, sns };
