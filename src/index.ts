6ba41ab91ee2c05e8f7a09549eb663a5c16fe6d8
Simsek Mert <simsek97@gmail.com>
2022-11-24 15:45:49 -0500

    Update exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
-import * as appflow from 'api/appflow';
-import * as cloudformation from 'api/cloudformation';
-import * as cloudwatch from 'api/cloudwatch';
-import * as iam from 'api/iam';
-import * as s3 from 'api/s3';
-import * as secretsmanager from 'api/secretsmanager';
-import * as sfn from 'api/sfn';
-import * as sns from 'api/sns';
-
-export { appflow, cloudformation, cloudwatch, iam, s3, secretsmanager, sfn, sns };
+export { describeConnectorEntity, describeConnectorProfiles } from './api/appflow';
+export {
+  createStack,
+  describeStack,
+  describeStackEvents,
+  describeStackResources,
+  enableTerminationProtection,
+  listStacks,
+  updateStack,
+} from './api/cloudformation';
+export { putDashboard } from './api/cloudwatch';
+export {
+  attachUserPolicy,
+  createAccessKey,
+  createUser,
+  deactivateAccessKey,
+  deleteAccessKey,
+  deleteUser,
+  listAccessKeys,
+  listUserGroups,
+  listUserInlinePolicies,
+  listUserPolicies,
+  listUsers,
+  updateAccessKey,
+} from './api/iam';
+export { getObject, listObjects, putObject } from './api/s3';
+export { getSecretValue } from './api/secretsmanager';
+export { describeExecution, getExecutionHistory, listExecutions, startExecution } from './api/sfn';
+export { subscribe } from './api/sns';
